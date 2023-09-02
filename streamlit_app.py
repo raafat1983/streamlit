2ecde77e0863a922a37226b257899f5597df946f
Ahmed Raafat <a.raafat@gmail.com>
2023-09-02 10:14:59 +0100

    update streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    #st.write(bytes_data)
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
@@ -25,5 +25,5 @@ if uploaded_file is not None:
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    #st.write(dataframe)
     #st.line_chart(dataframe) 
