43e3ff69bfb24b32d8909d2ebe2b603a2eea0c6d
Ahmed Raafat <a.raafat@gmail.com>
2023-09-02 10:10:32 +0100

    update streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+"""import streamlit as st
 import pandas as pd
  
 st.write("""
@@ -7,4 +7,27 @@ Hello *world!*
 """)
  
 df = pd.read_csv("my_data.csv")
-st.line_chart(df)
+st.line_chart(df) """
+
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+    st.line_chart(df) 
